3abd63048f9aae64d13cd755244da7bcec7038f8
湖南神雀网络科技有限公司 <69578571+sqqmall@users.noreply.github.com>
2021-03-04 20:29:41 +0800

    Update SignToolJSONObject.java
    数值型的字符串不截取

--- src/main/java/cn/com/sqqmall/integrals/sdk/SignToolJSONObject.java
+++ src/main/java/cn/com/sqqmall/integrals/sdk/SignToolJSONObject.java
@@ -95,21 +95,33 @@ public class SignToolJSONObject {
     
     /**
      * 去除首尾指定字符
-     * @param str   字符串
-     * @param element   指定字符
+     * 非数值型字符串才截取
+     * @param str
+     * @param element
      * @return
      */
     public static String trimFirstAndLastChar(String str, String element){
-        boolean beginIndexFlag = true;
-        boolean endIndexFlag = true;
-        do{
-            int beginIndex = str.indexOf(element) == 0 ? 1 : 0;
-            int endIndex = str.lastIndexOf(element) + 1 == str.length() ? str.lastIndexOf(element) : str.length();
-            str = str.substring(beginIndex, endIndex);
-            beginIndexFlag = (str.indexOf(element) == 0);
-            endIndexFlag = (str.lastIndexOf(element) + 1 == str.length());
-        } while (beginIndexFlag || endIndexFlag);
+        if(str instanceof String && !isInteger(str)){
+            if( str.length() > 2 ){
+                boolean beginIndexFlag = true;
+                boolean endIndexFlag = true;
+                do{
+                    int beginIndex = str.indexOf(element) == 0 ? 1 : 0;
+                    int endIndex = str.lastIndexOf(element) + 1 == str.length() ? str.lastIndexOf(element) : str.length();
+                    str = str.substring(beginIndex, endIndex);
+                    beginIndexFlag = (str.indexOf(element) == 0);
+                    endIndexFlag = (str.lastIndexOf(element) + 1 == str.length());
+                } while (beginIndexFlag || endIndexFlag);
+            }else{
+                str = "";
+            }
+        }
         return str;
+    }
+
+    public static boolean isInteger(String str) {
+        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+        return pattern.matcher(str).matches();
     }
 
 }
